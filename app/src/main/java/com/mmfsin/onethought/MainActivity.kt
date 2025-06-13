8f8a430105da03cd55ad793974704ceaca688bd4
gers <geraquem@gmail.com>
2025-06-13 12:41:02 +0200

    wip

---
+++ app/src/main/java/com/mmfsin/onethought/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mmfsin.onethought
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mmfsin.onethought.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/mmfsin/onethought/base/App.kt
@@ -0,0 +1,22 @@
+package com.mmfsin.onethought.base
+
+import android.app.Application
+
+//@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+//        Realm.init(this)
+//        MobileAds.initialize(this) {}
+
+//        getFCMToken()
+    }
+
+//    private fun getFCMToken() {
+//        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+//            if (it.isSuccessful) Log.i("FCM", it.result)
+//            else Log.i("FCM", "no token")
+//        }
+//    }
+}
